21db4857141da35cc0b34153b6e999b41721726b
Guilherme Rocha <luzguilherme.dev@gmail.com>
2025-03-21 15:41:33 -0300

    criando funções de acesso ao banco e adição de novos eventos

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,8 +1,10 @@
 import { db } from "./firebaseConfig";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, addDoc, collection, query, where } from "firebase/firestore";
+
+const eventsRef = collection(db, "eventos");
 
 const searchEvents = async () => {
-  const querySnapshot = await getDocs(collection(db, "eventos"));
+  const querySnapshot = await getDocs(eventsRef);
   const data = [];
   querySnapshot.forEach((doc) => {
     data.push(doc.data());
@@ -11,4 +13,22 @@ const searchEvents = async () => {
   return data;
 };
 
-export { searchEvents };
+const searchEventsByCategory = async (category) => {
+  const q = query(eventsRef, where("categoria", "==", category));
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+  });
+};
+
+const addNewEvent = async (event) => {
+  try {
+    const docRef = await addDoc(eventsRef, event);
+    console.log(`Novo evento adicionado com sucesso: ${docRef.id}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { searchEvents, searchEventsByCategory, addNewEvent };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,65 @@
-import { searchEvents } from "./firebase/firestore";
-const eventos = await searchEvents();
+import {
+  searchEvents,
+  searchEventsByCategory,
+  addNewEvent,
+} from "./firebase/firestore";
 
-const titulo = eventos[0].titulo;
-const descricao = eventos[0].descriçao;
-const data = eventos[0].data;
+// Listando TODOS os eventos existentes
+const listAllEvents = async () => {
+  const eventsContainer = document.getElementById("events");
+  const eventos = await searchEvents();
 
-document.getElementById("title").innerHTML = titulo;
-document.getElementById("desc").innerHTML = descricao;
-document.getElementById("data").innerHTML = data.toDate();
+  eventos.forEach((event) => {
+    // Criando os elementos
+    const li = document.createElement("li");
+    const title = document.createElement("h2");
+    const pDesc = document.createElement("p");
+    const pData = document.createElement("p");
+    const pEndereco = document.createElement("p");
+    const pCategoria = document.createElement("p");
+
+    // Adicionando os valores
+    title.textContent = event.titulo;
+    pDesc.textContent = event.descricao;
+    pData.textContent = `Data: ${event.data}`;
+    pEndereco.textContent = `Endereço: ${event.endereco}`;
+    pCategoria.textContent = `Categoria: ${event.categoria}`;
+
+    // Adicionando os elementos ao <li>
+    li.appendChild(title);
+    li.appendChild(pDesc);
+    li.appendChild(pData);
+    li.appendChild(pEndereco);
+    li.appendChild(pCategoria);
+
+    // Adicionando o <li> ao container
+    eventsContainer.appendChild(li);
+  });
+};
+
+const buttonEvent = document.getElementById("buttonEvents");
+
+buttonEvent.addEventListener("click", listAllEvents);
+
+// Adicionando novo evento
+formButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const tituloValue = document.getElementById("titulo").value;
+  const descricaoValue = document.getElementById("descricao").value;
+  const categoriaValue = document.getElementById("categoria").value;
+  const dateValue = document.getElementById("date").value;
+  const enderecoValue = document.getElementById("endereco").value;
+  const formButton = document.getElementById("formButton");
+
+  const newEvent = {
+    titulo: tituloValue,
+    descricao: descricaoValue,
+    categoria: categoriaValue,
+    data: dateValue,
+    endereco: enderecoValue,
+  };
+
+  console.log(newEvent);
+  addNewEvent(newEvent);
+});

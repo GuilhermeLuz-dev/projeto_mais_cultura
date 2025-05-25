70944fac295ce07e762587768b93fcbe9d39a4ae
Guilherme Rocha <luzguilherme.dev@gmail.com>
2025-05-25 00:32:29 -0300

    fix: resolvendo bug na página do evento

---
+++ dist/assets/formatDatas-DHqt_rn2.js
@@ -0,0 +1,1 @@
+const a=t=>{const e=["JAN","FEV","MAR","ABR","MAI","JUN","JUL","AGO","SET","OUT","NOV","DEZ"],o=t.getDate(),n=e[t.getMonth()],s=t.getFullYear();return{day:o,month:n,year:s}};export{a as f};
